d227c1e8dc5781cc8ea3fa9b008e25d3a1d77547
JSerGit <bibik.serej@gmail.com>
2022-05-28 21:57:11 +0300

    initial

---
+++ index.ts
@@ -0,0 +1,27 @@
+import inspector from 'node:inspector'
+import { watchProcessDeath } from 'watch-process-death'
+type Props = {
+    host?: string
+    port?: number
+}
+export default async ({
+    host = 'localhost',
+    port = 9229,
+}: Props): Promise<void> => {
+    const currentInspectorUrl = inspector.url()
+    console.log('currentInspectorUrl:', currentInspectorUrl)
+
+    const isInspectorStartedEarlier = !!currentInspectorUrl
+    if (isInspectorStartedEarlier) {
+        console.warn(
+            `Inspector (${host}:${port}) not started because he was started earler, and has url: ${currentInspectorUrl}`
+        )
+        return
+    }
+    inspector.open(port, host)
+    console.log('inspector.open')
+    // open -a "Google Chrome" http://stackoverflow.com
+    // open -a "Google Chrome" http://localhost:8081/web-downloads/
+    // --args --disable-web-security
+    watchProcessDeath(inspector.close)
+}
